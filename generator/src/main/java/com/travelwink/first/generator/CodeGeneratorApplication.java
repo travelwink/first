934c3ca927100cf18235bf95115faf13139a9124
min.liao <travelwink@163.com>
2023-04-27 01:36:55 +0800

    Add Generator Module
    For create mybatis-plus generator

---
+++ generator/src/main/java/com/travelwink/first/generator/CodeGeneratorApplication.java
@@ -0,0 +1,10 @@
+package com.travelwink.first.generator;
+
+/**
+ * @author chris
+ */
+public class CodeGeneratorApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
