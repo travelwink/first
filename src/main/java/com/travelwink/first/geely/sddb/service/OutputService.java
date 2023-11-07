bb33319419fff1a3939a34aed074152e2a008648
min.liao <travelwink@163.com>
2023-11-07 22:42:26 +0800

    Delete XML

--- src/main/java/com/travelwink/first/geely/sddb/service/OutputService.java
+++
@@ -1,9 +0,0 @@
-package com.travelwink.first.geely.sddb.service;
-
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-public interface OutputService {
-    void output(HttpServletResponse response, String projectId) throws IOException;
-}
--- src/main/java/com/travelwink/first/geely/sddb/service/ProjectService.java
+++
@@ -1,8 +0,0 @@
-package com.travelwink.first.geely.sddb.service;
-
-import com.travelwink.first.common.service.BaseService;
-import com.travelwink.first.geely.sddb.xmlNode.Project;
-
-public interface ProjectService extends BaseService<Project> {
-    Project getXmlInfo(String id);
-}
